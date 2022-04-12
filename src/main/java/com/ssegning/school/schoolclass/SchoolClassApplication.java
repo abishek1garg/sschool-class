00e0d96ac5a2d348c044d49a2d6eef8fc3c287ec
Stephane SEGNING LAMBOU <ssegning@yahoo.com>
2022-04-12 17:22:00 +0200

    Initial commit

---
+++ src/main/java/com/ssegning/school/schoolclass/SchoolClassApplication.java
@@ -0,0 +1,13 @@
+package com.ssegning.school.schoolclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolClassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssegning/school/schoolclass/SchoolClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssegning.school.schoolclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
